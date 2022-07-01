7132d2d02f29a10bb0e0089ce676d1259961dfe5
Khushpreet Singh <75271300+khushpreet-007@users.noreply.github.com>
2022-07-01 17:31:09 +0530

    Add files via upload

---
+++ codewithus/urls.py
@@ -0,0 +1,31 @@
+"""codewithkhushpreet URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from blog.views import frontpage, post_detail, about, contact, privacy, file, games
+
+urlpatterns = [
+    path('', frontpage, name='frontpage'),
+    path('admin/', admin.site.urls),
+    path('about/',about, name='about'), 
+    path('contact/',contact, name='contact'), 
+    path('privacy/',privacy, name='privacy'), 
+    path('file/',file, name='file'),  
+    path('games/',games, name='games'),    
+    path('<slug:slug>/', post_detail, name='post_detail'),
+  
+]
+
