f5e113280c5e2d4ba2bb7a076c4a0749a3a66794
Khushpreet Singh <75271300+khushpreet-007@users.noreply.github.com>
2022-07-04 16:39:34 +0530

    Update settings.py

--- codewithus/settings.py
+++ codewithus/settings.py
@@ -26,7 +26,10 @@ SECRET_KEY = 'django-insecure-jjy)5g27kr-u%2x5xnfa7fkg0uxp837ovyia&pyjzz8_r*yxy8
 DEBUG = True
 
 
-ALLOWED_HOSTS = ['studyanywhere.azurewebsites.net','studyanywhere.app', 'www.studyanywhere.app']
+# ALLOWED_HOSTS = ['studyanywhere.azurewebsites.net','studyanywhere.app', 'www.studyanywhere.app']
+
+
+# ALLOWED_HOSTS = ['*']
 
 # Application definition
 
