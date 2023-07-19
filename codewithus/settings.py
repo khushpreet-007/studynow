3dc3eedf3ba3c692bab0bedb0121234cf09361a1
Khushpreet Singh <75271300+khushpreet-007@users.noreply.github.com>
2023-07-19 15:56:06 +0530

    Update settings.py

--- codewithus/settings.py
+++ codewithus/settings.py
@@ -26,15 +26,18 @@ SECRET_KEY = 'django-insecure-jjy)5g27kr-u%2x5xnfa7fkg0uxp837ovyia&pyjzz8_r*yxy8
 DEBUG = True
 
 
-ALLOWED_HOSTS = ['studyanywhere.app', 'www.studyanywhere.app','127.0.0.1', 'studyanywhere.azurewebsites.net']
+# ALLOWED_HOSTS = ['studyanywhere.app', 'www.studyanywhere.app','127.0.0.1', 'studyanywhere.azurewebsites.net']
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # ALLOWED_HOSTS = ['*']
 
 # Application definition
 
-CSRF_TRUSTED_ORIGINS = ['https://studyanywhere.azurewebsites.net', 'https://www.studyanywhere.app','https://.127.0.0.1','https://studyanywhere.app/']
+# Application definition
+CSRF_TRUSTED_ORIGINS = ['https://.127.0.0.1']
 
+# CSRF_TRUSTED_ORIGINS = ['https://studyanywhere.azurewebsites.net', 'https://www.studyanywhere.app','https://.127.0.0.1','https://studyanywhere.app/']
 # CSRF_TRUSTED_ORIGINS = ['https://www.studyanywhere.app/','https://studyanywhere.azurewebsites.net/','https://.127.0.0.1']
 
 
